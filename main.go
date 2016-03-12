0c8618ad5e24f5de085b266a92f8d77ee49709d7
Andy Alness <andy.alness@gmail.com>
2016-03-11 19:17:32 -0800

    use redigo

--- main.go
+++ main.go
@@ -7,17 +7,14 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
-	"strings"
 	"time"
 
+	"github.com/garyburd/redigo/redis"
 	"github.com/golang/time/rate"
-	"gopkg.in/redis.v3"
 )
 
 var R = flag.Int("r", 0, "rate to produce messages (per second)")
 var N = flag.Int("n", 0, "number of topics")
-
-var RECEIVE_TIMEOUT_SECONDS = 2
 
 var total int
 var startTime time.Time
@@ -43,19 +40,20 @@ func main() {
 		fmt.Println("REDIS_HOST_ADDRESS must be set!")
 		return
 	}
-	client := redis.NewClient(&redis.Options{
-		Addr: address,
-	})
 
-	publisher := false
+	conn, err := redis.Dial("tcp", address)
+	if err != nil {
+		panic(err)
+	}
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
-	go func(){
+	go func() {
 		<-c
 		stop = true
-		if !publisher {
-			os.Exit(1)
-		}
+		rate := float64(total) / time.Now().Sub(startTime).Seconds()
+		fmt.Printf("total msgs processed: %d, achieved rate: %.2f msgs/sec\n", total, rate)
+		os.Exit(1)
 	}()
 
 	switch flag.Arg(0) {
@@ -64,35 +62,31 @@ func main() {
 			usage()
 			return
 		}
-		publisher = true
-		publish(client, *R, *N)
+		publish(conn, *R, *N)
+		rate := float64(total) / time.Now().Sub(startTime).Seconds()
+		fmt.Printf("total msgs processed: %d, achieved rate: %.2f msgs/sec\n", total, rate)
 	case "subscribe":
 		if *N == 0 {
 			usage()
 			return
 		}
-		subscribe(client, *N)
+		subscribe(conn, *N)
 	default:
 		usage()
-		return
 	}
-
-	offset := float64(RECEIVE_TIMEOUT_SECONDS)
-	if publisher {
-		offset = 0.0
-	}
-	rate := float64(total) / (time.Now().Sub(startTime).Seconds() - offset)
-	fmt.Printf("total msgs processed: %d, achieved rate: %.2f msgs/sec\n", total, rate)
 }
 
-func publish(client *redis.Client, r, n int) {
+func publish(conn redis.Conn, r, n int) {
 	limiter := rate.NewLimiter(rate.Limit(float64(r)), 100)
 	startTime = time.Now()
 	for !stop {
 		r := limiter.ReserveN(time.Now(), 1)
 		if r.OK() {
 			topic := strconv.Itoa(rand.Intn(n))
-			if err := client.Publish(topic, "derp").Err(); err != nil {
+			if err := conn.Send("PUBLISH", topic, "derp"); err != nil {
+				panic(err)
+			}
+			if err := conn.Flush(); err != nil {
 				panic(err)
 			}
 			total++
@@ -101,35 +95,26 @@ func publish(client *redis.Client, r, n int) {
 	}
 }
 
-func subscribe(client *redis.Client, n int) {
-	var pubsub *redis.PubSub
+func subscribe(conn redis.Conn, n int) {
+	pubsub := &redis.PubSubConn{conn}
 	for i := 0; i < n; i++ {
 		topic := strconv.Itoa(i)
-		var err error
-		if pubsub == nil {
-			pubsub, err = client.Subscribe(topic)
-		} else {
-			err = pubsub.Subscribe(topic)
-		}
-		if err != nil {
+		if err := pubsub.Subscribe(topic); err != nil {
 			panic(err)
+		}
+		switch msg := pubsub.Receive().(type) {
+		case error:
+			panic(msg)
 		}
 	}
 	fmt.Printf("subscribed to %d topics\n", n)
 	for {
+		switch msg := pubsub.Receive().(type) {
+		case error:
+			panic(msg)
+		}
 		if total == 0 {
-			if _, err := pubsub.ReceiveMessage(); err != nil {
-				panic(err)
-			}
 			startTime = time.Now()
-		} else {
-			if _, err := pubsub.ReceiveTimeout(time.Duration(RECEIVE_TIMEOUT_SECONDS) * time.Second); err != nil {
-				if strings.Contains(err.Error(), "i/o timeout") {
-					return
-				} else {
-					panic(err)
-				}
-			}
 		}
 		total++
 	}
