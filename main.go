6d1080622940c21b66f4d905cee5354b671c8789
Andy Alness <andy.alness@gmail.com>
2016-03-11 21:00:04 -0800

    synchronize exit

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 
 var R = flag.Int("r", 0, "rate to produce messages (per second)")
 var N = flag.Int("n", 0, "number of topics")
+var P = flag.Int("p", 0, "number of publishers")
 
 var total int
 var startTime time.Time
@@ -24,8 +25,8 @@ func usage() {
 	fmt.Printf("Usage:\n\n")
 	fmt.Printf("  publish messages at rate R randomly across N different topics:\n")
 	fmt.Printf("    -r <R> -n <N> publish\n\n")
-	fmt.Printf("  subscribe to N different topics:\n")
-	fmt.Printf("    -n <N> subscribe\n\n")
+	fmt.Printf("  subscribe to N different topics and expect P publishers:\n")
+	fmt.Printf("    -n <N> -p <P> subscribe\n\n")
 }
 
 func main() {
@@ -46,34 +47,38 @@ func main() {
 		panic(err)
 	}
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		<-c
-		stop = true
-		rate := float64(total) / time.Now().Sub(startTime).Seconds()
-		fmt.Printf("total msgs processed: %d, achieved rate: %.2f msgs/sec\n", total, rate)
-		os.Exit(1)
-	}()
-
 	switch flag.Arg(0) {
 	case "publish":
 		if *N == 0 || *R == 0 {
 			usage()
 			return
 		}
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		go func() {
+			<-c
+			stop = true
+		}()
 		publish(conn, *R, *N)
-		rate := float64(total) / time.Now().Sub(startTime).Seconds()
-		fmt.Printf("total msgs processed: %d, achieved rate: %.2f msgs/sec\n", total, rate)
+		// send exit
+		if err := conn.Send("PUBLISH", "0", "exit"); err != nil {
+			panic(err)
+		}
+		if err := conn.Flush(); err != nil {
+			panic(err)
+		}
 	case "subscribe":
 		if *N == 0 {
 			usage()
 			return
 		}
-		subscribe(conn, *N)
+		subscribe(conn, *N, *P)
 	default:
 		usage()
+		return
 	}
+	rate := float64(total) / time.Now().Sub(startTime).Seconds()
+	fmt.Printf("total msgs processed: %d, achieved rate: %.2f msgs/sec\n", total, rate)
 }
 
 func publish(conn redis.Conn, r, n int) {
@@ -83,7 +88,7 @@ func publish(conn redis.Conn, r, n int) {
 		r := limiter.ReserveN(time.Now(), 1)
 		if r.OK() {
 			topic := strconv.Itoa(rand.Intn(n))
-			if err := conn.Send("PUBLISH", topic, "derp"); err != nil {
+			if err := conn.Send("PUBLISH", topic, "update"); err != nil {
 				panic(err)
 			}
 			if err := conn.Flush(); err != nil {
@@ -95,8 +100,9 @@ func publish(conn redis.Conn, r, n int) {
 	}
 }
 
-func subscribe(conn redis.Conn, n int) {
+func subscribe(conn redis.Conn, n, p int) {
 	pubsub := &redis.PubSubConn{conn}
+	exit := 0
 	for i := 0; i < n; i++ {
 		topic := strconv.Itoa(i)
 		if err := pubsub.Subscribe(topic); err != nil {
@@ -112,6 +118,12 @@ func subscribe(conn redis.Conn, n int) {
 		switch msg := pubsub.Receive().(type) {
 		case error:
 			panic(msg)
+		case redis.Message:
+			if string(msg.Data) == "exit" {
+				if exit++; exit == p {
+					return
+				}
+			}
 		}
 		if total == 0 {
 			startTime = time.Now()
